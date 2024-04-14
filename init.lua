4f67e96046d63a86fa061b4c832ad1bc3d98cc7a
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-04-14 11:47:51 -0300

    Neovide support

--- init.lua
+++ init.lua
@@ -22,3 +22,7 @@ require("my.plugins.surround")
 require("my.plugins.tcomment")
 require("my.plugins.sneak")
 require("my.plugins.emmet")
+
+if vim.g.neovide then
+    require("my.neovide")
+end
---
+++ lua/my/neovide.lua
@@ -0,0 +1,6 @@
+-- Neovide
+vim.g.neovide_transparency = 0.9
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1b2c" })
+
+vim.o.guifont = "Fira Code:h12"
